0305118ff2367d58e937d0d3714473a21f8399d6
Neuter <mark@air.city>
2025-07-03 18:10:05 +0800

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'hki-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/favicon.ico',
+  '/hki-logo-black.png',
+  '/hki-logo-white.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return Promise.allSettled(
+          urlsToCache.map(url => cache.add(url).catch(err => {
+            console.warn(`Failed to cache ${url}:`, err);
+          }))
+        );
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
